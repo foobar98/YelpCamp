e02cb2b6c1fc587fc9bb088a69fe8c0d8f242f91
Pranav Gupta <pranavgupta998@gmail.com>
2017-08-12 18:50:48 +0000

    add mongoLab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
+//mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true});
+mongoose.connect("mongodb://pranav:password@ds055855.mlab.com:55855/yelp_camp", {useMongoClient: true});
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
